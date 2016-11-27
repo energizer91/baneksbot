6cecbe3cd4b647f330284647097eff3cd01f58b5
Alexander Bareyko <militryboy@yandex.ru>
2016-11-27 10:47:40 -0600

    Removed unused route

--- routes/aneks.js
+++ routes/aneks.js
@@ -4,13 +4,6 @@
 module.exports = function (express, mongo) {
     var router = express.Router();
 
-    router.get('/', function(req, res, next) {
-        /*return mysql.makeRequest('SELECT * FROM aneks').then(function (aneks) {
-            return res.json(aneks);
-        }).catch(next);*/
-        //res.render('index', { title: 'Express' });
-    });
-
     return {
         endPoint: '/aneks',
         router: router
