7616756ae7f33cb0a475e3c53df6881100e87026
Alexander Bareyko <militryboy@yandex.ru>
2016-11-27 16:39:54 -0600

    console traces

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
+      console.error(err, err.stack);
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -58,6 +59,7 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+    console.error(err, err.stack);
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
