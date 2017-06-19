bbb027e93b44cf50fac7feab173f453636b25ddc
Alexander Bareyko <militaryboy@yandex.ru>
2017-06-20 00:47:19 +0300

    date fix

--- routes/statistics.js
+++ routes/statistics.js
@@ -6,7 +6,7 @@ module.exports = function (express, botApi) {
     var router = express.Router();
 
     router.get('/users', (req, res, next) => {
-        return botApi.statistics.calculateStatistics(botApi.mongo, req.query.from ? new Date(req.query.from) : new Date().setHours(0, 0, 0, 0)).then(function (result) {
+        return botApi.statistics.calculateStatistics(botApi.mongo, req.query.from ? new Date(parseInt(req.query.from)) : new Date().setHours(0, 0, 0, 0)).then(function (result) {
             console.log(result);
 
             return res.json(result);
