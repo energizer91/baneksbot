cfa98ade049c76108199a836bd2e76eefe8fdaf8
Alexander Bareyko <militaryboy@yandex.ru>
2018-04-01 13:38:06 +0300

    improve stability

--- daemons/dbUpdater.js
+++ daemons/dbUpdater.js
@@ -26,74 +26,69 @@ var checkUpdateProgress = function (operation, ignoreUpdateProcess) {
         })
     },
     updateAneksTimer = function () {
-        return checkUpdateProgress('Initializing aneks update').then(function () {
-            return mongo.Anek.count();
-        }).then(function (count) {
-            return commonApi.redefineDatabase(count, mongo).then(commonApi.zipAneks);
-        }).then(function (aneks){
-            if (aneks.length) {
-                console.log(new Date(), aneks.length + ' aneks found. Start broadcasting');
-            } else {
-                console.log(new Date(), aneks.length + ' aneks found.');
-            }
-            return mongo.User.find({subscribed: true}).then(function (users) {
-                return commonApi.broadcastAneks(users, aneks, {_rule: 'common'}, mongo);
+        return checkUpdateProgress('Initializing aneks update')
+            .then(function () {
+                return mongo.Anek.count();
+            })
+            .then(function (count) {
+                return commonApi.redefineDatabase(count, mongo)
+                    .then(commonApi.zipAneks);
+            })
+            .then(function (aneks){
+                if (aneks.length) {
+                    console.log(new Date(), aneks.length + ' aneks found. Start broadcasting');
+                } else {
+                    console.log(new Date(), aneks.length + ' aneks found.');
+                }
+                return mongo.User.find({subscribed: true})
+                    .then(function (users) {
+                        return commonApi.broadcastAneks(users, aneks, {_rule: 'common'}, mongo);
+                    });
+            })
+            .catch(function (error) {
+                console.error(new Date(), 'An error occured: ' + error.message);
+            })
+            .then(function () {
+                console.log(new Date(), 'Updating aneks finished');
+                updateInProcess = false;
             });
-        }).catch(function (error) {
-            console.error(new Date(), 'An error occured: ' + error.message);
-        }).then(function () {
-            console.log(new Date(), 'Updating aneks finished');
-            updateInProcess = false;
-        });
     },
     updateLastAneksTimer = function () {
-        return checkUpdateProgress('Initializing last aneks update').then(function () {
-            return commonApi.getLastAneks(100, mongo);
-        }).catch(function (error) {
-            console.error(new Date(), 'An error occured: ' + error.message);
-        }).then(function () {
-            console.log(new Date(), 'Updating last aneks finished');
-            updateInProcess = false;
-        });
+        return checkUpdateProgress('Initializing last aneks update')
+            .then(function () {
+                return commonApi.getLastAneks(100, mongo);
+            })
+            .catch(function (error) {
+                console.error(new Date(), 'An error occured: ' + error.message);
+            })
+            .then(function () {
+                console.log(new Date(), 'Updating last aneks finished');
+                updateInProcess = false;
+            });
     },
     refreshAneksTimer = function () {
-        return checkUpdateProgress('Initializing aneks refresh').then(commonApi.updateAneks.bind(commonApi, mongo)).catch(function (error) {
-            console.error(new Date(), 'An error occured: ' + error.message);
-        }).then(function () {
-            console.log(new Date(), 'Refreshing aneks finished');
-            updateInProcess = false;
-        });
+        return checkUpdateProgress('Initializing aneks refresh')
+            .then(commonApi.updateAneks.bind(commonApi, mongo))
+            .catch(function (error) {
+                console.error(new Date(), 'An error occured: ' + error.message);
+            })
+            .then(function () {
+                console.log(new Date(), 'Refreshing aneks finished');
+                updateInProcess = false;
+            });
     },
     synchronizeDatabase = function () {
-        return checkUpdateProgress('Initializing aneks refresh', true).then(function () {
-            return new Promise(function (resolve, reject) {
-                if (configs.mongo.searchEngine !== 'elastic') {
-                    console.log('Database synchronizing is only available on elasticsearch engine');
-                    return;
-                }
-
-                var stream = mongo.Anek.synchronize(),
-                    count = 0;
-
-                updateInProcess = true;
-
-                stream.on('data', function () {
-                    count++;
-                });
-                stream.on('close', function () {
-                    return resolve(count);
-                });
-                stream.on('error', function (err) {
-                    return reject(err);
-                });
+        return checkUpdateProgress('Initializing aneks refresh', true)
+            .then(synchronizeWithElastic)
+            .then(function (count) {
+                console.log(new Date(), 'Successfully indexed', count, 'records');
+            })
+            .catch(function (error) {
+                console.error(new Date(), 'An error occured: ' + error.message);
+            })
+            .then(function () {
+                console.log(new Date(), 'Synchronize database finished');
             });
-        }).then(function (count) {
-            console.log(new Date(), 'Successfully indexed', count, 'records');
-        }).catch(function (error) {
-            console.error(new Date(), 'An error occured: ' + error.message);
-        }).then(function () {
-            console.log(new Date(), 'Synchronize database finished');
-        });
     },
     calculateStatisticsTimer = function () {
         return checkUpdateProgress('Initializing statistics calculate', true).then(function () {
@@ -149,5 +144,35 @@ process.on('message', function(m) {
         }
     }
 });
+
+function synchronizeWithElastic() {
+    return new Promise(function (resolve, reject) {
+        if (configs.mongo.searchEngine !== 'elastic') {
+            console.log('Database synchronizing is only available on elasticsearch engine');
+            return;
+        }
+
+        var stream,
+            count = 0;
+
+        try {
+            stream = mongo.Anek.synchronize();
+        } catch (err) {
+            return reject(err);
+        }
+
+        updateInProcess = true;
+
+        stream.on('data', function () {
+            count++;
+        });
+        stream.on('close', function () {
+            return resolve(count);
+        });
+        stream.on('error', function (err) {
+            return reject(err);
+        });
+    });
+}
 
 process.send({ message: 'ready' });
